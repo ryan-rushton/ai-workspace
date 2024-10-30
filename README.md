0ce55d5e50661ba413b54fbfca26ac082256490a
Ryan <ryan.rushton79@gmail.com>
2024-10-30 21:39:40 +1000

    Generate some captions

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ Since each tool has its own environment, it is recommended to have separate acti
 
 ## Install
 
+Windows Git LFS wasn't working out of the box for me so I needed to run `git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"` to set up on windows.
+
 1. Pull all the submodules with `git submodule update --init --recursive`.
-1. Git LFS wasn't working out of the box for me so I needed to run `git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"` to set up on windows.
+1. Setup a python venv with `python -m venv .venv`.
+1. Install torch with cuda `pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124`.
+1. Run `.venv/bin/activate`
+1. Install non torch deps `pip install -r requirements.txt`
 
 ## TODO
 
--- src/scripts/generate_captions.py
+++ src/scripts/generate_captions.py
@@ -11,9 +11,9 @@ from transformers import AutoProcessor, AutoModelForCausalLM
 image_extensions = (".png", ".jpg")
 
 
-def write_caption_file(image_path: str, caption: str):
-    file_name = os.path.splitext(image_path)[0]
-    with open(f"{file_name}.txt", "w") as file:
+def write_caption_file(dir: str, image_filename: str, caption: str):
+    file_name = os.path.splitext(image_filename)[0]
+    with open(os.path.join(dir, f"{file_name}.txt"), "w") as file:
         # Write text to the file
         file.write(caption)
 
@@ -23,29 +23,32 @@ def read_image_files(dir: str) -> list[str]:
 
 
 def create_captions(dir: str, concept_sentence: str):
-    print(f"run_captioning")
+    print(f"create captions dir={dir} concept_sentence={concept_sentence}")
     print(f"concept sentence {concept_sentence}")
     # Load internally to not consume resources for training
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"device={device}")
+    if not torch.cuda.is_available():
+        print("Cuda is not available for torch")
+        raise RuntimeError("Cuda is not available for this torch install.")
+
     torch_dtype = torch.float16
     model = AutoModelForCausalLM.from_pretrained(
         "multimodalart/Florence-2-large-no-flash-attn",
         torch_dtype=torch_dtype,
         trust_remote_code=True,
-    ).to(device)
+    ).to("cuda")
     processor = AutoProcessor.from_pretrained(
         "multimodalart/Florence-2-large-no-flash-attn", trust_remote_code=True
     )
 
     images_files = read_image_files(dir=dir)
 
-    for i, image_path in enumerate(images_files):
+    for i, image_filename in enumerate(images_files):
+        image_path = os.path.join(dir, image_filename)
         image = Image.open(image_path).convert("RGB")
 
         prompt = "<DETAILED_CAPTION>"
         inputs = processor(text=prompt, images=image, return_tensors="pt").to(
-            device, torch_dtype
+            "cuda", torch_dtype
         )
         print(f"inputs {inputs}")
 
@@ -72,7 +75,7 @@ def create_captions(dir: str, concept_sentence: str):
         if concept_sentence:
             caption_text = f"{concept_sentence} {caption_text}"
 
-        write_caption_file(image_path=image_path, caption=caption_text)
+        write_caption_file(dir=dir, image_filename=image_filename, caption=caption_text)
 
     model.to("cpu")
     del model
@@ -86,4 +89,4 @@ if __name__ == "__main__":
     parser.add_argument("image_dir", help="The path to the image directory")
     parser.add_argument("concept", help="The concept sentence, unique or rare word")
     args = parser.parse_args()
-    create_captions(image_dir=args.image_dir, concept_sentence=args.concept)
+    create_captions(dir=args.image_dir, concept_sentence=args.concept)
